5d639e0b90bdfb303b39c3a0fadeda9ae2b9649f
Annapurna13 <yoavgecht@gmail.com>
2018-05-01 10:39:52 +0300

    updates

--- server/apis/link-shortner-controller.js
+++ server/apis/link-shortner-controller.js
@@ -71,8 +71,7 @@ const getUrl = (req, res) => {
            if(doc) {
                 console.log('REDIRECTING')
                 console.log('doc.long_url', doc.long_url);
-                res.header("Access-Control-Allow-Origin", "*");
-                res.header("Access-Control-Allow-Headers", "X-Requested-With");
+                res.header("Access-Control-Allow-Headers", "x-requested-with, x-requested-by");
                 res.writeHead(302, {'Location': `https://cors-anywhere.herokuapp.com/${doc.long_url}`});
 				res.end();
            } else {
